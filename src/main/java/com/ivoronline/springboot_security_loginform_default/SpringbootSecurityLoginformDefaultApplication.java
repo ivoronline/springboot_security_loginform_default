3006f0e001c5250dd8b78625bcfe77072770ac46
ivoronline <ivoronline@gmail.com>
2022-01-22 22:21:21 +0100

    Commit

--- src/main/java/com/ivoronline/springboot_security_loginform_default/SpringbootSecurityLoginformDefaultApplication.java
+++ src/main/java/com/ivoronline/springboot_security_loginform_default/SpringbootSecurityLoginformDefaultApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
-@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SpringbootSecurityLoginformDefaultApplication {
 
   public static void main(String[] args) {
--- src/main/java/com/ivoronline/springboot_security_loginform_default/controllers/MyController.java
+++ src/main/java/com/ivoronline/springboot_security_loginform_default/controllers/MyController.java
@@ -1,18 +1,20 @@
 package com.ivoronline.springboot_security_loginform_default.controllers;
 
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class MyController {
 
   //==============================================================================
   // HELLO (Secured Resource)
   //==============================================================================
   @Secured("ROLE_USER")
-  @RequestMapping("/Hello")
-  public String hello() {
+  @RequestMapping("Hello")
+   String hello() {
     return "Hello from Controller";
   }
 
